22134bf347b9a32523575ecdd957437906d6edc3
kauderk <estudio1kau@gmail.com>
2024-01-07 12:20:12 -0500

    add: string.template single quotes textMateRules

--- src/extension/settings.ts
+++ src/extension/settings.ts
@@ -29,14 +29,20 @@ const textMateRules = [
   },
   {
     name: extensionId + 'quote.begin',
-    scope: ['punctuation.definition.string.begin.tsx'],
+    scope: [
+      'punctuation.definition.string.begin.tsx',
+      'punctuation.definition.string.template.begin.tsx',
+    ],
     settings: {
       foreground: '#b5a90000',
     },
   },
   {
     name: extensionId + 'quote.end',
-    scope: ['punctuation.definition.string.end.tsx'],
+    scope: [
+      'punctuation.definition.string.end.tsx',
+      'punctuation.definition.string.template.end.tsx',
+    ],
     settings: {
       foreground: '#b5030000',
     },
